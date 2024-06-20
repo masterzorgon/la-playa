cc6edd70bc68920e4bc5360de052b39fc973d247
Nathan Galindo <nathan.zebedee@gmail.com>
2024-06-20 01:50:50 -0500

    change catering emails

--- src/app/api/catering/route.ts
+++ src/app/api/catering/route.ts
@@ -32,8 +32,9 @@ export async function POST(request: NextRequest) {
         // send notification email to la playa
         const { data: notifData } = await resend.emails.send({
             from: "laplaya@laplayamexicancafe.com",
-            to: "Laplayamain@gmail.com",
-            cc: ["elizabeth@laplayamexicancafe.com",],
+            to: "nathan.zebedee@gmail.com",
+            // to: "Laplayamain@gmail.com",
+            // cc: ["elizabeth@laplayamexicancafe.com",],
             subject: "New Catering Request!",
             react: NotificationCateringRequest({
                 firstName,
