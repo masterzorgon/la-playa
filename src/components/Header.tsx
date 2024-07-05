84313217fcccd0f3c4cd9d77f3eb3964ae44fea2
Nathan Galindo <nathan.zebedee@gmail.com>
2024-07-05 11:27:25 -0500

    edit menu button color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -73,7 +73,7 @@ export function Header() {
               {({ open }) => (
                 <>
                   <Popover.Button
-                    className="relative z-10 -m-2 inline-flex items-center rounded-lg stroke-gray-900 p-2 hover:bg-gray-200/50 hover:stroke-gray-600 active:stroke-gray-900 ui-not-focus-visible:outline-none"
+                    className="relative z-10 -m-2 inline-flex items-center rounded-lg stroke-white p-2 hover:bg-gray-200/50 hover:stroke-gray-600 active:stroke-gray-900 ui-not-focus-visible:outline-none"
                     aria-label="Toggle site navigation"
                   >
                     {({ open }) =>
