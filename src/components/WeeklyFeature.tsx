aabab5a67ae6ce56123dd2b85397bb2a24b74b1e
Nathan Galindo <nathan.zebedee@gmail.com>
2024-11-18 09:25:29 -0600

    hotfix

--- src/components/WeeklyFeature.tsx
+++ src/components/WeeklyFeature.tsx
@@ -49,19 +49,19 @@ export function WeeklyFeature() {
             <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
                     <img 
-                        src={weeklyFeature && weeklyFeature.entry.fields.image.fields.file.url}
+                        src={weeklyFeature?.entry?.fields?.image?.fields?.file?.url || ""}
                         className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
                         alt=""
                     />
                     <div className="h-96 w-full flex-auto">
                         <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                            <span>{weeklyFeature && weeklyFeature.entry.fields.title}</span>
+                            {weeklyFeature?.entry?.fields?.title || "Loading..."}
                         </h2>
                         <p className='mt-10 text-white text-2xl font-semibold'>
-                            {weeklyFeature && weeklyFeature.entry.fields.subTitle} 
+                            {weeklyFeature?.entry?.fields?.subTitle || "Loading..."} 
                         </p>
                         <p className="mt-2 text-lg leading-8 text-gray-200">
-                            {weeklyFeature && weeklyFeature.entry.fields.description} 
+                            {weeklyFeature?.entry?.fields?.description || "Loading..."} 
                         </p>
                         <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
                             <Button
